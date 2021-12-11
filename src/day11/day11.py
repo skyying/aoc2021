3da7d70ce6e33eb56374c4f9413ed36656047738
skyying <dreamuk@gmail.com>
2021-12-11 17:46:19 +0800

    add day 11

---
+++ src/day11/day11.py
@@ -0,0 +1,72 @@
+import sys
+
+sys.path.append('../../')
+from src.utils.utils import *
+
+adjacent_units = [(1, 0), (-1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+
+
+def get_inputs():
+    return [mapl(lambda x: int(x), line.strip()) for line in open('in')]
+
+
+def is_valid_pos(grid, pos):
+    x, y = pos
+    return 0 <= y < len(grid) and 0 <= x < len(grid[0])
+
+
+def print_grid(grid):
+    print('\n'.join([','.join(mapl(lambda c: str(c), s)) for s in grid]))
+
+
+def flush_octopuses(grid):
+    flushed = set()
+    [Octopus(grid, (x, y), flushed).step() for x in range(len(grid[0])) for y in range(len(grid))]
+    return len(flushed)
+
+
+class Octopus:
+    def __init__(self, grid, pos, flushed):
+        self.grid = grid
+        self.pos = pos
+        self.neighbors = self.get_neighbors()
+        self.flushed = flushed
+
+    def get_neighbors(self):
+        x, y = self.pos
+        return [(dx + x, dy + y) for dx, dy in adjacent_units if is_valid_pos(self.grid, (dx + x, dy + y))]
+
+    def flush(self):
+        x, y = self.pos
+        self.grid[y][x] = 0
+        self.flushed.add(self.pos)
+
+    def step(self):
+        x, y = self.pos
+        if self.pos not in self.flushed:
+            self.grid[y][x] += 1
+        flush_boundary = 10
+        if self.grid[y][x] == flush_boundary:
+            self.flush()
+            for neighbor_pos in self.neighbors:
+                Octopus(self.grid, neighbor_pos, self.flushed).step()
+
+
+def part_one(step):
+    grid = get_inputs()
+    return sum([flush_octopuses(grid) for _ in range(step)])
+
+
+def part_two():
+    step = 1
+    grid = get_inputs()
+    while True:
+        flushed_count = flush_octopuses(grid)
+        if flushed_count == len(grid) * len(grid[0]):
+            break
+        step += 1
+    return step
+
+
+print(part_one(100))
+print(part_two())
